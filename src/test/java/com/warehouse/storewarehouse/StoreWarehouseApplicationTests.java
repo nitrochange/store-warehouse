bcbed3dbb84d532bc8106d6f690ae9b36824511b
Alex <sasha.sanya.romanov@mail.ru>
2021-10-12 11:25:01 +0300

    Initial commit

---
+++ src/test/java/com/warehouse/storewarehouse/StoreWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warehouse.storewarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
