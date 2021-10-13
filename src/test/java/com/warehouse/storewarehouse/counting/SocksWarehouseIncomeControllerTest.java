073186b73adf5fdaea6a03379e9c369a2e7744a8
Alex <sasha.sanya.romanov@mail.ru>
2021-10-13 18:27:41 +0300

    Добавлены интеграционные тесты эндпоинтов по отдельности и сервиса в целом.

--- src/test/java/com/warehouse/storewarehouse/counting/SocksWarehouseIncomeControllerTest.java
+++ src/test/java/com/warehouse/storewarehouse/counting/SocksWarehouseIncomeControllerTest.java
@@ -4,7 +4,6 @@ package com.warehouse.storewarehouse.counting;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -16,12 +15,8 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
 import static org.assertj.core.api.BDDAssertions.then;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @ExtendWith(SpringExtension.class)
@@ -38,20 +33,7 @@ public class SocksWarehouseIncomeControllerTest {
     @Autowired
     private JacksonTester<DeliveryBatchSocks> jsonRequestAttempt;
 
-    @Mock
-    private SocksRepository socksRepository;
-
     private final String urlTemplate = "/api/socks/income";
-
-    @BeforeEach
-    public void setUp() {
-        socksService = new SocksServiceImpl(
-                socksRepository
-        );
-
-        given(socksRepository.save(any())).will(returnsFirstArg());
-    }
-
 
     @Test
     public void postValidResult() throws Exception {
@@ -68,35 +50,4 @@ public class SocksWarehouseIncomeControllerTest {
         then(response.getStatus()).isEqualTo(HttpStatus.OK.value());
     }
 
-    @Test
-    public void postInvalidResult() throws Exception {
-
-        //given deliveries with incorrect data
-        DeliveryBatchSocks wrongDelivery1 = new DeliveryBatchSocks(null, 10,10);
-        DeliveryBatchSocks wrongDelivery2 = new DeliveryBatchSocks("Yellow", -100,10);
-        DeliveryBatchSocks wrongDelivery3 = new DeliveryBatchSocks("Black", 20,-37);
-
-
-        MockHttpServletResponse response = mvc.perform(
-                post(urlTemplate).contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonRequestAttempt.write(wrongDelivery1).getJson())
-        ).andReturn().getResponse();
-
-
-        then(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-
-        response = mvc.perform(
-                post(urlTemplate).contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonRequestAttempt.write(wrongDelivery2).getJson())
-        ).andReturn().getResponse();
-
-        then(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-
-        response = mvc.perform(
-                post(urlTemplate).contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonRequestAttempt.write(wrongDelivery3).getJson())
-        ).andReturn().getResponse();
-
-        then(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-    }
 }
